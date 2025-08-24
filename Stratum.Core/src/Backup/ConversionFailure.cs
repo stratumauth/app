3b504a7a22f501f7070c2f4f5a4c146ff6697502
helpgod11111 <helpgod11111@gmail.com>
2025-08-25 05:13:40 +0800

    Delete Stratum.Core directory

--- Stratum.Core/src/Backup/ConversionFailure.cs
+++
@@ -1,8 +0,0 @@
-namespace Stratum.Core.Backup
-{
-    public class ConversionFailure
-    {
-        public string Description { get; set; }
-        public string Error { get; set; }
-    }
-}
--- Stratum.Core/src/Persistence/ICategoryRepository.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (C) 2022 jmh
-// SPDX-License-Identifier: GPL-3.0-only
-
-using Stratum.Core.Entity;
-
-namespace Stratum.Core.Persistence
-{
-    public interface ICategoryRepository : IAsyncRepository<Category, string>;
-}
