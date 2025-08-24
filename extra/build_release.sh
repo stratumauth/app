e900d6e4b00a09c76858ee03ccfdb23c97d3b524
helpgod11111 <helpgod11111@gmail.com>
2025-08-25 05:46:42 +0800

    Delete extra directory

--- extra/build_release.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/bin/env python3 build.py android apk "$@"
-/usr/bin/env python3 build.py android apk --fdroid "$@"
-/usr/bin/env python3 build.py android apk --fdroid --runtime android-arm "$@"
-/usr/bin/env python3 build.py android apk --fdroid --runtime android-arm64 "$@"
-/usr/bin/env python3 build.py android aab "$@"
-/usr/bin/env python3 build.py wearos apk "$@"
-/usr/bin/env python3 build.py wearos aab "$@"
